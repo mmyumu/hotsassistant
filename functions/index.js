f6782505f4be6b6758027809f76c69ae6ceed5af
LABETAA <martin.labetaa@sogeti.com>
2017-12-11 19:30:48 +0100

    Improve function to manage synergies, maps

--- functions/index.js
+++ functions/index.js
@@ -20,34 +20,140 @@ const functions = require('firebase-functions');
 const HERO_PARAM = 'hero';
 
 // API.AI Intent names
-const COUNTER_INTENT = 'hotsassistant.counter';
+const HERO_INFO_INTENT = 'hotsassistant.heroInfo';
+const REASON_INTENT = 'hotsassistant.reason';
+const MAP_INFO_INTENT = 'hotsassistant.mapInfo';
+
+const COUNTER_ACTION = 'contrer';
+const SYNERGIE_ACTION = 'synergie';
+
+const FORT_LEVEL = 'fort';
+const FAIBLE_LEVEL = 'faible';
+
+const DATABASE_FILE = 'db.json';
+
+// Message patterns
+const COUNTER_SPEECH_PATTERN = `{
+   count, plural,
+	=0 {Il n'y a pas de contre de {hero}.}
+	one {Le contre de {hero} est {heroes}}
+	other {Les contres de {hero} sont {heroes}}
+}`;
+const SYNERGIE_SPEECH_PATTERN = `{
+	count, plural,
+	=0 {{hero} n'a de synergie avec aucun autre héro.}
+	other {{hero} a une synergie avec {heroes}}
+}`;
+const MAP_SPEECH_PATTERN = `{
+	count, plural,
+	=0 {Il n'y a pas de {level} map pour {hero}.}
+	one{La {level} map de {hero} est {maps}.}
+	other {Les {level}s maps de {hero} sont {maps}}
+}`;
+const LEVEL_PATTERN = ` {
+	level, select,
+	fort {bonne}
+	faible {mauvaise}
+	other {level}
+}`;
+const ERROR_PATTERN = `Je ne comprends pas ce que vous voulez dire par {action}`;
+
 
 var fs = require('fs');
+var formatMessage = require('format-message');
 
 exports.hotsassistant = functions.https.onRequest((request, response) => {
-  const app = new DialogflowApp({request, response});
-  console.log('Request headers: ' + JSON.stringify(request.headers));
-  console.log('Request body: ' + JSON.stringify(request.body));
+	const app = new DialogflowApp({request, response});
+	console.log('Request headers: ' + JSON.stringify(request.headers));
+	console.log('Request body: ' + JSON.stringify(request.body));
 
-  // Fulfill action business logic
-  function counter (app) {
-	fs.readFile('db.json', 'utf8', function(err, contents) {
-		if (err) {
-			return console.log(err);
-		}
-		var root = JSON.parse(contents);
-		
-		var heroName = request.body.result.parameters.hero;
-		
-		console.log(root.heroes[heroName]);
-		app.tell(JSON.stringify(root.heroes[heroName].reason));
-	});
-	
-  }
+	// Fulfill action business logic
+	function heroInfo(app) {
+		fs.readFile(DATABASE_FILE, 'utf8', (err, contents) => {
+			if (err) {
+				return console.log(err);
+			}
+			var root = JSON.parse(contents);
+			
+			var heroName = request.body.result.parameters.hero;
+			var action = request.body.result.parameters.action;
 
-  const actionMap = new Map();
-  // actionMap.set('input.welcome', responseHandler);
-  actionMap.set(COUNTER_INTENT, counter);
+			var heroKey = heroName.toLowerCase();
+			
+			
+			if(action == COUNTER_ACTION) {
+				var heroes = root.heroes[heroKey].counters;
+				var speechPattern = COUNTER_SPEECH_PATTERN;
+			} else if(action == SYNERGIE_ACTION) {
+				var heroes = root.heroes[heroKey].synergies;
+				var speechPattern = SYNERGIE_SPEECH_PATTERN;
+			} else {
+				return app.ask(formatMessage(ERROR_PATTERN, {action:action}));
+			}
+				
+			var heroesAsString = heroes.map(elem => elem.name).join(', ');
+			var msg = formatMessage(speechPattern, { hero: heroName, count:heroes.length, heroes: heroesAsString});
+			app.ask(msg);
+		});
+	}
+  
+	function reason(app) {
+		fs.readFile(DATABASE_FILE, 'utf8', (err, contents) => {
+			if (err) {
+				return console.log(err);
+			}
+			var root = JSON.parse(contents);
+			
+			var heroName = request.body.result.parameters.hero;
+			var action = request.body.result.parameters.action;
+			
+			var heroKey = heroName.toLowerCase();	
+			
+			if(action == COUNTER_ACTION) {
+				var reason = root.heroes[heroKey].counterReason;
+			} else if(action == SYNERGIE_ACTION) {
+				var reason = root.heroes[heroKey].synergieReason;
+			} else {
+				return app.ask(formatMessage(ERROR_PATTERN, {action:action}));
+			}
 
-  app.handleRequest(actionMap);
+			app.ask(reason);
+		});
+	}
+  
+	function mapInfo(app) {
+		fs.readFile(DATABASE_FILE, 'utf8', (err, contents) => {
+			if (err) {
+				return console.log(err);
+			}
+			var root = JSON.parse(contents);
+			
+			var heroName = request.body.result.parameters.hero;
+			var level = request.body.result.parameters.level;
+
+			var heroKey = heroName.toLowerCase();
+			
+			
+			if(level == FORT_LEVEL) {
+				var maps = root.heroes[heroKey].mapsStronger;
+			} else if(level == FAIBLE_LEVEL) {
+				var maps = root.heroes[heroKey].mapsWeaker;
+			} else {
+				return app.ask(formatMessage(ERROR_PATTERN, {level:level}));
+			}
+				
+			var mapsAsString = maps.map(elem => elem.name).join(', ');
+			var levelAsString = formatMessage(LEVEL_PATTERN, {level: level});
+			var msg = formatMessage(MAP_SPEECH_PATTERN, { hero: heroName, level:levelAsString , count:maps.length, maps: mapsAsString});
+			app.ask(msg);
+		});
+	}
+
+	const actionMap = new Map();
+	// actionMap.set('input.welcome', responseHandler);
+	actionMap.set(HERO_INFO_INTENT, heroInfo);
+	actionMap.set(REASON_INTENT, reason);
+	actionMap.set(MAP_INFO_INTENT, mapInfo);
+
+	app.handleRequest(actionMap);
 });
