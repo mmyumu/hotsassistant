17b3d69ddbda7e944c63eff09bfad2a21e6adb32
mmyumu <mmyumu@gmail.com>
2018-02-24 14:30:50 +0100

    Add eslint for code quality

--- functions/speechPatterns.js
+++ functions/speechPatterns.js
@@ -5,55 +5,55 @@ const LEVEL_PATTERN = 'level';
 const ERROR_PATTERN = 'error';
 const ANYTHING_ELSE_PATTERN = 'anythingElse';
 
-var speechPatterns = { "fr": {}, "en":{}};
+var speechPatterns = { 'fr': {}, 'en': {} };
 speechPatterns['fr'][COUNTER_PATTERN] = `{
    count, plural,
-	=0 {Il n'y a pas de contre de {hero}. }
-	one {Le contre de {hero} est {heroes}. }
-	other {Les contres de {hero} sont {heroes}. }
+    =0 {Il n'y a pas de contre de {hero}. }
+    one {Le contre de {hero} est {heroes}. }
+    other {Les contres de {hero} sont {heroes}. }
 }`;
 speechPatterns['fr'][SYNERGIE_PATTERN] = `{
-	count, plural,
-	=0 {{hero} n'a de synergie avec aucun autre héro. }
-	other {{hero} a une synergie avec {heroes}. }
+    count, plural,
+    =0 {{hero} n'a de synergie avec aucun autre héro. }
+    other {{hero} a une synergie avec {heroes}. }
 }`;
 speechPatterns['fr'][MAP_PATTERN] = `{
-	count, plural,
-	=0 {Il n'y a pas de {level} map pour {hero}. }
-	one{La {level} map de {hero} est {maps}. }
-	other {Les {level}s maps de {hero} sont {maps}. }
+    count, plural,
+    =0 {Il n'y a pas de {level} map pour {hero}. }
+    one{La {level} map de {hero} est {maps}. }
+    other {Les {level}s maps de {hero} sont {maps}. }
 }`;
 speechPatterns['fr'][LEVEL_PATTERN] = ` {
-	level, select,
-	fort {bonne}
-	faible {mauvaise}
-	other {level}
+    level, select,
+    fort {bonne}
+    faible {mauvaise}
+    other {level}
 }`;
 speechPatterns['fr'][ERROR_PATTERN] = `Je ne comprends pas ce que vous voulez dire par {action}. `;
 speechPatterns['fr'][ANYTHING_ELSE_PATTERN] = `Voulez-vous savoir autre chose ? `;
 
 speechPatterns['en'][COUNTER_PATTERN] = `{
    count, plural,
-	=0 {There is no counter for {hero}. }
-	one {The counter for {hero} is {heroes}. }
-	other {The counters for {hero} are {heroes}. }
+    =0 {There is no counter for {hero}. }
+    one {The counter for {hero} is {heroes}. }
+    other {The counters for {hero} are {heroes}. }
 }`;
 speechPatterns['en'][SYNERGIE_PATTERN] = `{
-	count, plural,
-	=0 {{hero} has no synergy. }
-	other {{hero} has synergy with {heroes}. }
+    count, plural,
+    =0 {{hero} has no synergy. }
+    other {{hero} has synergy with {heroes}. }
 }`;
 speechPatterns['en'][MAP_PATTERN] = `{
-	count, plural,
-	=0 {Il There is no {level} map for {hero}. }
-	one{The {level} map for {hero} is {maps}. }
-	other {The {level} maps for {hero} are {maps}. }
+    count, plural,
+    =0 {Il There is no {level} map for {hero}. }
+    one{The {level} map for {hero} is {maps}. }
+    other {The {level} maps for {hero} are {maps}. }
 }`;
 speechPatterns['en'][LEVEL_PATTERN] = ` {
-	level, select,
-	fort {good}
-	faible {bad}
-	other {level}
+    level, select,
+    fort {good}
+    faible {bad}
+    other {level}
 }`;
 speechPatterns['en'][ERROR_PATTERN] = `I do not understand what do you mean by {action}. `;
 speechPatterns['en'][ANYTHING_ELSE_PATTERN] = `Is there anything else you want to know? `;
@@ -67,7 +67,7 @@ speechPatterns['en-ca'] = speechPatterns['en'];
 speechPatterns['en-in'] = speechPatterns['en'];
 
 module.exports = {
-	get: function(lang) {
-		return speechPatterns[lang];
-	}
-}
+  get: function (lang) {
+    return speechPatterns[lang];
+  }
+};
